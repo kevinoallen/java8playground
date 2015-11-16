8652e880f5bde2e7b620cdafe7feed5c7ccba274
Allen, Kevin <kevinoallen@gmail.com>
2015-11-16 16:29:25 -0500

    Compare lists in Read Resource Test

--- koautils/src/test/java/com/koatest/utils/ResourceReadTest.java
+++ koautils/src/test/java/com/koatest/utils/ResourceReadTest.java
@@ -1,11 +1,14 @@
 package com.koatest.utils;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 @Test
 public class ResourceReadTest {
@@ -22,13 +25,14 @@ public class ResourceReadTest {
 	public Object[][] getTestDP() {
 		return new Object[][] {
 			{null, new String[] {"Fred","John","Francine","Franco","Jeffery","George","Gina","Turk","Mathew"}},
-			{"[F]\\.*", new String[] {"Fred","Francine","Franco"}},
+			{"F.*", new String[] {"Fred","Francine","Franco"}},
 		};
 	}
 	
 	@Test(dataProvider="resourceTestDP")
 	public void testResourceRead(String regex, String[] expected) throws IOException {
-		List<String> lines = fixture.readLines("resources.txt", regex);
-		lines.forEach(System.out::println);
+		List<String> actual = fixture.readLines("resources.txt", regex);
+		assertEquals(actual, Arrays.asList(expected));
+		actual.forEach(System.out::println);
 	}
 }
